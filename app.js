288f1af96e4ab14687a0d127ec8b2a6f6bf07856
andrea <dev.mixicruz@gmail.com>
2024-01-20 01:29:58 -0600

    sube los cambios del template string con el operdor ternario

--- app.js
+++ app.js
@@ -3,13 +3,12 @@
 let numeroSecreto = 6;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraveces = 'vez';
 let maximosintentos = 3;
 while (numeroUsuario != numeroSecreto){
     numeroUsuario = prompt('Elige un número entre 1 y 10')
     
     if (numeroUsuario == numeroSecreto){
-        alert(`Acertaste, el número secreto es: ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraveces}`);
+        alert(`Acertaste, el número secreto es: ${numeroSecreto}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
     } else {
         if (numeroUsuario > numeroSecreto){
             alert('El numero secreto es menor');
@@ -17,7 +16,6 @@ while (numeroUsuario != numeroSecreto){
             alert('El numero secreto es mayor');     
         }
         intentos++;
-        palabraveces = 'veces';
         if (intentos > maximosintentos ) {
             alert(`Llegaste al número máximo de ${maximosintentos} intentos permitidos`);
             break;
